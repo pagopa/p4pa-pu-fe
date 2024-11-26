c3d8c258c37bd909b83e8d6e70016a565c2e5313
Vito Falco <vitofalco@gmail.com>
2024-11-26 17:38:26 +0100

    Manage subpath on the dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const DEPLOY_PATH = process.env.DEPLOY_PATH || ''
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: `${DEPLOY_PATH}`,
   plugins: [react()],
   server: {
     port: 1234,
