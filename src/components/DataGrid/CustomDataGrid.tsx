96a01270b005e269e0252caee8a121cbfddb02f0
Alessio Bilello <alessio.bilello@emeal.nttdata.com>
2025-01-29 17:35:31 +0100

    update CustomDataGrid

--- src/components/DataGrid/CustomDataGrid.tsx
+++ src/components/DataGrid/CustomDataGrid.tsx
@@ -1,6 +1,7 @@
 import { styled } from '@mui/material';
 import { DataGrid, DataGridProps, GridColDef, GridValidRowModel } from '@mui/x-data-grid';
 import { theme } from '@pagopa/mui-italia';
+import CustomPagination from './CustomPagination';
 
 const StyledDataGrid = styled(DataGrid)({
   border: 'none !important',
@@ -16,16 +17,33 @@ const StyledDataGrid = styled(DataGrid)({
 interface CustomDataGridProps<T extends GridValidRowModel> extends Omit<DataGridProps, 'rows' | 'columns'> {
   rows: T[];
   columns: GridColDef[];
+  customPagination?: {
+    sizePageOptions?: number[];
+    defaultPageOption?: number;
+    totalPages?: number;
+  }
 }
 
 const CustomDataGrid = <T extends GridValidRowModel>({
   rows,
   columns,
+  customPagination,
   ...restProps
 }: CustomDataGridProps<T>) => (
     <StyledDataGrid
       rows={rows}
       columns={columns}
+      pagination
+      disableRowSelectionOnClick
+      slots={{
+        pagination: () => (
+          <CustomPagination
+            sizePageOptions={customPagination?.sizePageOptions}
+            defaultPageOption={customPagination?.defaultPageOption}
+            totalPages={customPagination?.totalPages}
+          />
+        )
+      }}
       {...restProps}
     />
   );
