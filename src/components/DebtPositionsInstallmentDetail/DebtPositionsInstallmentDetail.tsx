17cf4b8541e397959515ff0d3e1472fc206b2b01
Alessio Bilello <alessio.bilello@emeal.nttdata.com>
2025-03-03 17:45:57 +0100

    minor refactor DebtPositionsInstallmentDetail

--- src/components/DebtPositionsInstallmentDetail/DebtPositionsInstallmentDetail.tsx
+++ src/components/DebtPositionsInstallmentDetail/DebtPositionsInstallmentDetail.tsx
@@ -6,43 +6,38 @@ import DetailContainer, { DetailData } from '../DetailContainer/DetailContainer'
 import EmptyDetailContainer from './EmptyDetailContainer';
 import { InstallmentDTO } from '../../../generated/apiClient';
 
+type DetailDataValue = Record<string, DetailData[]> | DetailData[];
+/*TMP MOCK DATA*/
+const summaryTitleMock: string = 'Saldo Tari 2025';
+export const tmpMockData: DetailDataValue = {
+  summaryData: [
+    { label: 'Stato', value: 'PAID', chipConfig: {color: 'default', variant: 'outlined'} },
+    { label: 'Codice Avviso (IUV)', value: '0300330000000001', variant: 'monospaced' },
+    { label: 'Importo', value: '50,00 €' },
+    { label: 'Data scadenza', value: '24/03/2025' },
+    { label: 'Debitore', value: 'Maria Bianchi' },
+    { label: 'CF / Partita IVA', value: 'BNCMRA82B42C933X (Persona fisica)' },
+    { label: 'Tipo dovuto', value: 'TARI' },
+  ],
+  paymentData: [
+    { label: 'Data esito', value: '01/09/2024' },
+    { label: 'Eseguito da', value: 'Paolo Rossi' },
+    { label: 'CF / Partita IVA', value: 'PLRSRA82B42C933X (Persona fisica)' },
+    { label: 'Gestore della transazione (PSP)', value: 'POSTMAN_TEST' },
+    { label: 'IUD', value: '000a99aa114e6b142268f27abb8b347c37d' },
+    { label: 'IUR', value: 'hR3sT2uG888KkKK' },
+  ]
+};
 
 export const DebtPositionsInstallmentDetail = () => {
 
   type DebtStatus = Pick<InstallmentDTO, 'status'>['status'];
-
   const DEBT_RESOLVED_STATES: DebtStatus[] = ['PAID', 'REPORTED'];
-  const DEBT_PENDING_STATES: DebtStatus[] = ['UNPAID', 'EXPIRED', 'INVALID', 'CANCELLED'];
-
-  /*START - TMP MOCK DATA*/
-  type DetailDataValue = Record<string, DetailData[]> | DetailData[];
-  const summaryTitleMock: string = 'Saldo Tari 2025';
-  const tmpMockData: DetailDataValue = {
-    summaryData: [
-      { label: 'Stato', value: 'PAID', chipConfig: {color: 'default', variant: 'outlined'} },
-      { label: 'Codice Avviso (IUV)', value: '0300330000000001', variant: 'monospaced' },
-      { label: 'Importo', value: '50,00 €' },
-      { label: 'Data scadenza', value: '24/03/2025' },
-      { label: 'Debitore', value: 'Maria Bianchi' },
-      { label: 'CF / Partita IVA', value: 'BNCMRA82B42C933X (Persona fisica)' },
-      { label: 'Tipo dovuto', value: 'TARI' },
-    ],
-    paymentData: [
-      { label: 'Data esito', value: '01/09/2024' },
-      { label: 'Eseguito da', value: 'Paolo Rossi' },
-      { label: 'CF / Partita IVA', value: 'PLRSRA82B42C933X (Persona fisica)' },
-      { label: 'Gestore della transazione (PSP)', value: 'POSTMAN_TEST' },
-      { label: 'IUD', value: '000a99aa114e6b142268f27abb8b347c37d' },
-      { label: 'IUR', value: 'hR3sT2uG888KkKK' },
-    ]
-  };
 
   const { t } = useTranslation();
 
   const currentState = tmpMockData.summaryData.find(item => item.label === 'Stato')?.value as DebtStatus;
-
   const isResolved = DEBT_RESOLVED_STATES.includes(currentState);
-  const isPending = DEBT_PENDING_STATES.includes(currentState);
 
   return (
     <>
@@ -76,7 +71,7 @@ export const DebtPositionsInstallmentDetail = () => {
           />
         </Grid>
         <Grid item md={6}>
-          {isResolved && (
+          {isResolved ? (
             <DetailContainer 
               sections={[{
                 title: {label: t('installmentDetailPage.paymentInformation'), variant: 'overline'},
@@ -84,11 +79,7 @@ export const DebtPositionsInstallmentDetail = () => {
                 divider: true
               }]}
             />
-          )}
-          
-          {isPending && (
-            <EmptyDetailContainer />
-          )}
+          ) : (<EmptyDetailContainer />)}
         </Grid>
       </Grid>
       <Divider orientation="horizontal" flexItem sx={{ display: 'block', mt: 3}}/>
