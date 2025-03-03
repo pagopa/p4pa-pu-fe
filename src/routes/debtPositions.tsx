03c4dcbf16655bd1ad2be47d618392a7dbc3d026
Alessio Bilello <alessio.bilello@emeal.nttdata.com>
2025-03-03 09:09:26 +0100

    add path route

--- src/routes/debtPositions.tsx
+++ src/routes/debtPositions.tsx
@@ -3,6 +3,7 @@ import { Layout } from '../components/layout/Layout';
 import { RouteHandleObject } from '../models/Breadcrumbs';
 import config from '../utils/config';
 import { DebtPositionsResults } from './DebtPositions/DebtPositionsResults';
+import DebtPositionsInstallmentDetail from './DebtPositionsInstallmentDetail';
 
 const deployPath = config.deployPath;
 
@@ -25,6 +26,11 @@ export const debtPositionsRoutes = [
         id: 'DEBT_POSITIONS_RESULTS',
         path: 'results',
         element: <DebtPositionsResults />,
+      },
+      {
+        id: 'DEBT_POSITION_INSTALLMENT_DETAIL',
+        path: 'installment-detail',
+        element: <DebtPositionsInstallmentDetail />,
       }
     ]
   }
