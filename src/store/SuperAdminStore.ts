4fb8ed5eb81aec57038912fbf72a4994997b0be2
Vito Falco <vitofalco@gmail.com>
2025-03-04 16:47:58 +0100

    Drop superadmin as store. Add a check to set suepradmin as const

--- src/store/SuperAdminStore.ts
+++
@@ -1,8 +0,0 @@
-import { signal } from '@preact/signals-react';
-
-export const superAdminState = signal<boolean>(false);
-
-export function setSuperAdmin(superAdmin: boolean) {
-  superAdminState.value = superAdmin;
-}
-
