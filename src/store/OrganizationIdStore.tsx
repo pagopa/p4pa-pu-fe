f6a77930ee8d1c949d2318533208eb550abb555f
Vito Falco <vitofalco@gmail.com>
2025-03-07 09:01:30 +0100

    Fix build

--- src/store/OrganizationIdStore.tsx
+++ src/store/OrganizationIdStore.tsx
@@ -4,12 +4,12 @@ import { OrganizationIdMemo } from '../models/Organization';
 import { STATE } from './types';
 
 // Initialize the persistent store
-export const organizationIdState = usePersistentSignal<OrganizationIdMemo | undefined>(
+export const organizationIdState = usePersistentSignal<OrganizationIdMemo>(
   STATE.ORGANIZATION_ID,
   { storage: sessionStorage }
 );
 
 // Function to update the organizationId
-export function setOrganizationId(organizationId: OrganizationIdMemo | undefined) {
+export function setOrganizationId(organizationId: OrganizationIdMemo) {
   organizationIdState.state.value = organizationId;
 }
