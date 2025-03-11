38785e67e4297fc2500891030a7a6ab26a8edc3f
Vito Falco <vitofalco@gmail.com>
2025-03-11 13:26:02 +0100

    Add token management

---
+++ src/routes/AuthCallback/index.test.tsx
@@ -0,0 +1,27 @@
+import { afterEach, describe, it, vi } from 'vitest';
+import AuthCallback from './index';
+import { render } from '@testing-library/react';
+
+vi.mock('react-router', async (importOriginal) => ({
+  ...(await importOriginal()),
+  useLoaderData: vi.fn( () => ({ access_token: 'mocked-access-token' }) )
+}));
+
+
+describe('AutchCallback Page', () => {
+
+  afterEach(() => {
+        vi.clearAllMocks();
+  });
+      
+  it('renders Auth callback without crashing', async () => {
+    globalThis.location = {
+      ...globalThis.location,
+      replace: vi.fn()
+    };
+    
+    render(
+      <AuthCallback />
+          );
+  });
+});
