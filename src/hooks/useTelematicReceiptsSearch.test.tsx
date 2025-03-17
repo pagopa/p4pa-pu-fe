556d125685ed05dffdb7e65456db3a6d6753378f
Vito Falco <vitofalco@gmail.com>
2025-03-17 17:44:59 +0100

    Add hooks tests

---
+++ src/hooks/useTelematicReceiptsSearch.test.tsx
@@ -0,0 +1,41 @@
+import { act } from 'react';
+import { describe, expect, it } from 'vitest';
+import { renderHook } from '../__tests__/renderers';
+import useTelematicReceiptSearch, {
+  TelematicReceiptFilters
+} from './useTelematicReceiptsSearch';
+
+describe('useTelematicReceiptSearch', () => {
+  const initialFilters: TelematicReceiptFilters = {
+    dateRange: {
+      from: new Date('2024-01-01'),
+      to: new Date('2024-12-31')
+    }
+  };
+
+  it('should initialize with default values', () => {
+    const { result } = renderHook(() =>
+      useTelematicReceiptSearch({
+        initialFilters
+      })
+    );
+
+    expect(result.current.filterValues).toEqual(initialFilters);
+    expect(result.current.pagination.page).toBe(0);
+    expect(result.current.pagination.size).toBe(10);
+  });
+
+  it('should update filter values on handleFilterChange', () => {
+    const { result } = renderHook(() =>
+      useTelematicReceiptSearch({
+        initialFilters
+      })
+    );
+
+    act(() => {
+      result.current.handleFilterChange('iuv', '123');
+    });
+
+    expect(result.current.filterValues.iuv).toBe('123');
+  });
+});
