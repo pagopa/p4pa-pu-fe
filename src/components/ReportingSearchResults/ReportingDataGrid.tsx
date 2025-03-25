7a82772f422fc6c85b0524ef14934207f5c91720
luigilesca <luigi.lesca@nttdata.com>
2025-03-25 10:00:57 +0100

    add data on to table and start handling table pagination

--- src/components/ReportingSearchResults/ReportingDataGrid.tsx
+++ src/components/ReportingSearchResults/ReportingDataGrid.tsx
@@ -9,60 +9,82 @@ import CustomDataGrid from '../DataGrid/CustomDataGrid';
 import { FileDownload, Visibility } from '@mui/icons-material';
 import { generatePath, useNavigate } from 'react-router-dom';
 import { PageRoutes } from '../../App';
+import { PagedPaymentsReportingView } from '../../../generated/data-contracts';
+import { moneyFormat } from '../../utils/formatters';
 
 type SearchResultDataRow = {
   id: number;
   idReporting: string;
   idRegulation: string;
   regulationDate: string;
-  flowDate: string;
-  payments: string;
-  totalAmount: string;
+  flowDateTime: string;
+  totalPayments: string;
+  totalAmountCents: string;
 } & GridValidRowModel;
 
-const SearchResultsDataGrid = () => {
+export type DataGridProps = {
+  data: PagedPaymentsReportingView;
+  onPageChange: (page: number) => void;
+  onPageSizeChange: (page: number) => void;
+  onSortChange: (model: Array<string>) => void;
+  pagination: {
+    currentPage: number;
+    page: number;
+    size: number;
+  };
+};
+
+const SearchResultsDataGrid = ({
+  data,
+  onPageChange,
+  onPageSizeChange,
+  onSortChange,
+  pagination
+}: DataGridProps) => {
   const { t } = useTranslation();
   const navigate = useNavigate();
 
-  const rows: Array<SearchResultDataRow> = [
-    {
-      id: 1,
-      idReporting: '2024-11-10123531',
-      idRegulation: '49445-2454456456',
-      regulationDate: '05/11/2021',
-      flowDate: '05/11/2021 04:06:44',
-      payments: '100',
-      totalAmount: '100,00 €'
-    },
-    {
-      id: 2,
-      idReporting: '2024-11-10123531',
-      idRegulation: '50445-2454456456',
-      regulationDate: '06/11/2021',
-      flowDate: '06/11/2021 05:06:44',
-      payments: '200',
-      totalAmount: '200,00 €'
-    },
-    {
-      id: 3,
-      idReporting: '2024-12-10123531',
-      idRegulation: '514453-2454456456',
-      regulationDate: '07/11/2021',
-      flowDate: '07/11/2021 06:06:44',
-      payments: '300',
-      totalAmount: '300,00 €'
-    }
-  ];
+  console.log('data', data);
+
+  // const rows: Array<SearchResultDataRow> = [
+  //   {
+  //     id: 1,
+  //     idReporting: '2024-11-10123531',
+  //     idRegulation: '49445-2454456456',
+  //     regulationDate: '05/11/2021',
+  //     flowDateTime: '05/11/2021 04:06:44',
+  //     totalPayments: '100',
+  //     totalAmountCents: '100,00 €'
+  //   },
+  //   {
+  //     id: 2,
+  //     idReporting: '2024-11-10123531',
+  //     idRegulation: '50445-2454456456',
+  //     regulationDate: '06/11/2021',
+  //     flowDateTime: '06/11/2021 05:06:44',
+  //     totalPayments: '200',
+  //     totalAmountCents: '200,00 €'
+  //   },
+  //   {
+  //     id: 3,
+  //     idReporting: '2024-12-10123531',
+  //     idRegulation: '514453-2454456456',
+  //     regulationDate: '07/11/2021',
+  //     flowDateTime: '07/11/2021 06:06:44',
+  //     totalPayments: '300',
+  //     totalAmountCents: '300,00 €'
+  //   }
+  // ];
 
   const columns: Array<GridColDef> = [
     {
-      field: 'idReporting',
+      field: 'iuf',
       headerName: t('reportingSearchResults.searchReportingId'),
       flex: 1,
       type: 'string'
     },
     {
-      field: 'idRegulation',
+      field: 'regulationUniqueIdentifier',
       headerName: t('reportingSearchResults.searchRegulationId'),
       flex: 1,
       type: 'string'
@@ -71,25 +93,31 @@ const SearchResultsDataGrid = () => {
       field: 'regulationDate',
       headerName: t('reportingSearchResults.regulationDate'),
       flex: 1,
-      type: 'string'
+      type: 'string',
+      renderCell: (params: GridRenderCellParams) =>
+        params.value ? new Date(params.value).toLocaleDateString('it-IT') : ''
     },
     {
-      field: 'flowDate',
+      field: 'flowDateTime',
       headerName: t('reportingSearchResults.flowDate'),
       flex: 1,
-      type: 'string'
+      type: 'string',
+      renderCell: (params: GridRenderCellParams) =>
+        params.value ? new Date(params.value).toLocaleDateString('it-IT') : ''
     },
     {
-      field: 'payments',
+      field: 'totalPayments',
       headerName: t('reportingSearchResults.payments'),
       flex: 1,
       type: 'string'
     },
     {
-      field: 'totalAmount',
+      field: 'totalAmountCents',
       headerName: t('reportingSearchResults.totalAmount'),
       flex: 1,
-      type: 'string'
+      type: 'string',
+      renderCell: (params: GridRenderCellParams<SearchResultDataRow>) =>
+        moneyFormat(params.value as number)
     },
     {
       field: 'action',
@@ -108,14 +136,18 @@ const SearchResultsDataGrid = () => {
               action: () =>
                 navigate(
                   generatePath(PageRoutes.REPORTING_DETAIL, {
-                    id: params.row.idReporting
+                    id: params.row.ingestionFlowFileId
                   })
                 )
             },
             {
               icon: <FileDownload fontSize="small" />,
               label: t('commons.files.download'),
-              action: () => console.log('Scarica file per ID: ', params.row.id)
+              action: () =>
+                console.log(
+                  'Scarica file per ID: ',
+                  params.row.ingestionFlowFileId
+                )
             }
           ]}
         />
@@ -126,12 +158,18 @@ const SearchResultsDataGrid = () => {
   return (
     <>
       <CustomDataGrid
-        rows={rows}
+        rows={data?.content ?? []}
+        getRowId={(row) => row.ingestionFlowFileId}
         columns={columns}
         disableColumnMenu
         disableColumnResize
         customPagination={{
-          totalPages: 10
+          defaultPageOption: pagination.size,
+          sizePageOptions: [5, 10, 20],
+          totalPages: data?.totalPages,
+          currentPage: pagination.currentPage,
+          onPageChange,
+          onPageSizeChange
         }}
       />
     </>
--- src/components/ReportingSearchResults/ReportingSearchResults.tsx
+++ src/components/ReportingSearchResults/ReportingSearchResults.tsx
@@ -1,23 +1,126 @@
-import { Grid, useTheme } from '@mui/material';
-import { Search } from '@mui/icons-material';
+import { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
-import SearchResultsDataGrid from './ReportingDataGrid';
-import TitleComponent from '../TitleComponent/TitleComponent';
+import { Grid, Stack, useTheme } from '@mui/material';
+import { Search } from '@mui/icons-material';
 import FilterContainer, {
   COMPONENT_TYPE
 } from '../FilterContainer/FilterContainer';
+import TitleComponent from '../TitleComponent/TitleComponent';
+import SearchResultsDataGrid from './ReportingDataGrid';
+import { BaseFilterValues } from '../../models/Filters';
+import { PagedPaymentsReportingView } from '../../../generated/data-contracts';
+import useReportingSearch from '../../hooks/useReportingSearch';
+
+export type LocationState = {
+  filters: BaseFilterValues;
+};
 
 const ReportingSearchResults = () => {
+  console.log('REPORTING SEARCH RESULTS');
   const theme = useTheme();
   const { t } = useTranslation();
+  // const {
+  //   state: { filters: initialFilters }
+  // }: { state: LocationState } = useLocation();
+  console.log('Location:', location);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const pageFromUrl = parseInt(searchParams.get('page') || '1'); // default = 1
+  const sizeFromUrl = parseInt(searchParams.get('size') || '10');
+
+  const reportingSearch = useReportingSearch({
+    // initialFilters
+    initialFilters: {
+      // regulationDateFrom: new Date('2024-01-01'),
+      // regulationDateTo: new Date('2024-03-31')
+      // regulationUniqueIdentifier: '1201240790068119'
+    },
+    initialPage: pageFromUrl - 1, // parte da 0
+    initialSize: sizeFromUrl
+  });
+  console.log('reportingSearch', reportingSearch);
+
+  useEffect(() => {
+    const totalPages = reportingSearch.query.data?.totalPages;
+    const currentDataPage = reportingSearch.pagination.page; // paginazione  inizia da 0
+    const currentDataSize = reportingSearch.pagination.size;
+    const currentUrlPage = parseInt(searchParams.get('page') || '1'); // paginazione url inizia da 1
+
+    console.log('currentDataPage', currentDataPage);
+    console.log('currentUrlPage', currentUrlPage);
+    console.log('totalPages', totalPages);
+
+    if (totalPages !== undefined && currentUrlPage > totalPages) {
+      // Evitare loop: aggiornare solo se la URL è disallineata dallo stato
+      if (currentDataPage !== 0) {
+        reportingSearch.handlePageChange(0);
+      }
+      // Creo una copia dei parametri URL attuali
+      const params = new URLSearchParams(searchParams);
+      console.log('currentDataSize', currentDataSize);
+      params.set('page', '1');
+      params.set('size', String(currentDataSize));
+      // Usare replace per evitare "indietro"
+      setSearchParams(params, { replace: true });
+    }
+  }, [reportingSearch.query.data?.totalPages]);
+
+  const handlePageChange = (newPage: number) => {
+    setSearchParams((prev) => {
+      const params = new URLSearchParams(prev);
+      params.set('page', String(newPage));
+      return params;
+    });
+    reportingSearch.handlePageChange(newPage);
+  };
+
+  const handlePageSizeChange = (newSize: number) => {
+    setSearchParams((prev) => {
+      const params = new URLSearchParams(prev);
+      params.set('size', String(newSize));
+      return params;
+    });
+    reportingSearch.handlePageSizeChange(newSize);
+  };
 
   return (
-    <>
+    <Stack>
       <TitleComponent
         title={t('commons.routes.REPORTING_SEARCH_RESULTS')}
         description={t('reportingSearchResults.description')}
       />
-      <Grid>
+
+      <Stack gap={3}>
+        {/* <FilterContainer
+          items={filters}
+          values={telematicReceipts.filterValues}
+          onChange={telematicReceipts.handleFilterChange}
+        /> */}
+        <Grid
+          container
+          p={2}
+          height="100%"
+          sx={{
+            bgcolor: theme.palette.grey[200],
+            overflow: 'auto'
+          }}
+          aria-label="results-table"
+        >
+          <SearchResultsDataGrid
+            data={reportingSearch.query.data as PagedPaymentsReportingView}
+            // onPageChange={reportingSearch.handlePageChange}
+            // onPageSizeChange={reportingSearch.handlePageSizeChange}
+            onPageChange={handlePageChange}
+            onPageSizeChange={handlePageSizeChange}
+            onSortChange={reportingSearch.setSort}
+            pagination={reportingSearch.pagination}
+          />
+        </Grid>
+      </Stack>
+
+      {/* <Grid>
         <Grid
           container
           direction="row"
@@ -70,8 +173,8 @@ const ReportingSearchResults = () => {
         >
           <SearchResultsDataGrid />
         </Grid>
-      </Grid>
-    </>
+      </Grid> */}
+    </Stack>
   );
 };
 
