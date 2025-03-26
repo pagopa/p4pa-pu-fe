2878755435c344c0e3ca17339a52273a0eec5e1b
Vito Falco <vitofalco@gmail.com>
2025-03-26 16:17:56 +0100

    Treasuries api call

---
+++ src/api/treasuries.ts
@@ -0,0 +1,35 @@
+import { useMutation } from '@tanstack/react-query';
+import utils from '../utils';
+
+type TreasuriesParams = Parameters<
+  typeof utils.apiClient.bff.getTreasuries
+>;
+export type TreasuriesQuery = TreasuriesParams[1];
+
+export type TreasuriesRequest = {
+  organizationId: TreasuriesParams[0];
+  query: TreasuriesQuery;
+};
+
+const getTreasuries = ({
+    organizationId
+  }: {
+    organizationId: TreasuriesRequest['organizationId'];
+  }) =>
+    useMutation({
+      mutationKey: ['getTreasuries', organizationId],
+      mutationFn: async (query: TreasuriesQuery) => {
+        const { data: response } = await utils.apiClient.bff.getTreasuries(
+          organizationId,
+          query,
+          {
+            paramsSerializer: {
+              // repeat array params as query string
+              indexes: null
+            }
+          }
+        );
+  
+        return response;
+      }
+    });
