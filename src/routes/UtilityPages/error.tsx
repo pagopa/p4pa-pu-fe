fe4e11f40e971d49b1838f34e5f14bf050d978c8
EmVal <147710433+EmanueleValentini1@users.noreply.github.com>
2025-04-17 11:09:59 +0200

    feat: [P4ADEV-1843] get organization unhappy path (#163)

---
+++ src/routes/UtilityPages/error.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+
+export const ErrorPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        flexDirection={'column'}
+      >
+        <Typography
+          component="h1"
+          variant="h3"
+          color="textPrimary"
+          gutterBottom
+        >
+          {t('utilityPages.error.title')}
+        </Typography>
+        <Typography variant="h6" color="textSecondary" gutterBottom>
+          {t('utilityPages.error.subtitle')}
+        </Typography>
+        <Link to={'/home'}>{t('utilityPages.error.retry')}</Link>
+      </Box>
+    </>
+  );
+};
+
+export default ErrorPage;
---
+++ src/utils/navigation.ts
@@ -0,0 +1,40 @@
+import config from './config';
+
+const deployPath = config.deployPath || '';
+
+const routes = {
+  LOGGED_OUT: `${deployPath}/loggedout`,
+  ERROR: `${deployPath}/error`
+};
+
+let authErrorInProgress = false;
+
+const setAuthErrorState = (state: boolean) => {
+  authErrorInProgress = state;
+};
+
+const isAuthErrorInProgress = () => authErrorInProgress;
+
+const navigateTo = (route: string) => {
+  window.location.replace(route);
+};
+
+const navigateToLoggedOut = () => {
+  setAuthErrorState(true);
+  navigateTo(routes.LOGGED_OUT);
+};
+
+const navigateToError = () => {
+  if (!isAuthErrorInProgress()) {
+    navigateTo(routes.ERROR);
+  }
+};
+
+export default {
+  routes,
+  navigateTo,
+  navigateToLoggedOut,
+  navigateToError,
+  setAuthErrorState,
+  isAuthErrorInProgress
+};
