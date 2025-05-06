d5eb6540433a0cdba38db63321372083904d2dc7
luigilesca <luigi.lesca@nttdata.com>
2025-05-06 16:21:27 +0200

    remove cooment code

--- src/routes/DebtPositionCreateWizard/components/Beneficiary/BeneficiaryFieldControls/BeneficiaryPaymentFields.tsx
+++ src/routes/DebtPositionCreateWizard/components/Beneficiary/BeneficiaryFieldControls/BeneficiaryPaymentFields.tsx
@@ -11,7 +11,6 @@ export function BeneficiaryPaymentFields<T extends FieldValues>({
   fieldNamePrefix,
   validationContext,
   disabled,
-  // getValues,
   trigger,
   errors,
   fieldValidators,
@@ -45,21 +44,7 @@ export function BeneficiaryPaymentFields<T extends FieldValues>({
             validate: {
               ibanFormat: fieldValidators.validateIBAN,
               paymentMethod: (value: string) => {
-                // const postalAccount = getValues(
-                //   buildBeneficiaryFieldPath<T, 'postalAccount'>(
-                //     fieldNamePrefix,
-                //     index,
-                //     'postalAccount'
-                //   )
-                // );
-
                 const result = fieldValidators.validatePaymentMethod(value);
-
-                // Se uno dei due campi ha un valore, non mostrare errori
-                // if (
-                //   (value && value.trim() !== '') ||
-                //   (postalAccount && postalAccount.trim() !== '')
-                // ) {
 
                 // Se l'IBAN ha un valore, non mostrare errori
                 if (value && value.trim() !== '') {
@@ -84,60 +69,6 @@ export function BeneficiaryPaymentFields<T extends FieldValues>({
           )}
         />
       </Grid>
-
-      {/* Postal Account field - Temporarily disabled
-      <Grid item xs={12}>
-        <BeneficiaryControlledField<T>
-          name={buildBeneficiaryFieldPath<T, 'postalAccount'>(
-            fieldNamePrefix,
-            index,
-            'postalAccount'
-          )}
-          control={control}
-          rules={{
-            validate: {
-              postalAccountFormat: fieldValidators.validatePostalAccount,
-              paymentMethod: (value: string) => {
-                const iban = getValues(
-                  buildBeneficiaryFieldPath<T, 'iban'>(
-                    fieldNamePrefix,
-                    index,
-                    'iban'
-                  )
-                );
-
-                const result = fieldValidators.validatePaymentMethod(
-                  iban,
-                  value
-                );
-
-                // Se uno dei due campi ha un valore, non mostrare errori
-                if (
-                  (value && value.trim() !== '') ||
-                  (iban && iban.trim() !== '')
-                ) {
-                  return undefined;
-                }
-
-                return result;
-              }
-            }
-          }}
-          renderField={({ field }) => (
-            <PostalAccountField
-              field={field}
-              t={t}
-              disabled={disabled}
-              context={validationContext}
-              index={index}
-              trigger={trigger}
-              fieldNamePrefix={fieldNamePrefix}
-              errors={errors}
-            />
-          )}
-        />
-      </Grid>
-      */}
     </>
   );
 }
