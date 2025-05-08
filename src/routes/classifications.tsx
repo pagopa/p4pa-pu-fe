e3bbc71265ee79908793bcd55a7fdfec00cd10b2
abilello1 <164199664+abilello1@users.noreply.github.com>
2025-05-08 16:08:26 +0200

    feat: [P4ADEV-2677] add classifications export overview (#188)

--- src/routes/classifications.tsx
+++ src/routes/classifications.tsx
@@ -2,6 +2,7 @@ import config from '../utils/config';
 import { Layout } from '../components/layout/Layout';
 import { RouteHandleObject } from '../models/Routes';
 import { Classifications } from '../components/Classifications';
+import ClassificationsOverview from './ClassificationsOverview';
 
 const deployPath = config.deployPath;
 
@@ -18,6 +19,14 @@ export const classificationsRoutes = [
         handle: {
           backButton: false,
           hideBreadcrumbs: true
+        } as RouteHandleObject
+      },
+      {
+        id: 'CLASSIFICATIONS_SEARCH_RESULTS',
+        path: 'search-results',
+        element: <ClassificationsOverview />,
+        handle: {
+          backButton: true
         } as RouteHandleObject
       }
     ]
