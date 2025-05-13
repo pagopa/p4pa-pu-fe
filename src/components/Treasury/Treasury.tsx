2f3abe4eddae5edd455150f515bc58ba1cd7708d
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-05-13 09:55:40 +0200

    [P4ADEV-2612] remove unused filter values and console logs in Treasury component

--- src/components/Treasury/Treasury.tsx
+++ src/components/Treasury/Treasury.tsx
@@ -10,20 +10,11 @@ import { useMultiFilters } from '../../hooks/useMultiFilters';
 
 export const Treasury = () => {
   const { t } = useTranslation();
-  const {
-    filterMap,
-    removeAllFilters,
-    noFilterIsSelected,
-    filterValues,
-    selectedFilters
-  } = useMultiFilters({
+  const { filterMap, removeAllFilters, noFilterIsSelected } = useMultiFilters({
     clearOnMount: true
   });
   const navigate = useNavigate();
 
-  console.log('filterMap', filterMap);
-  console.log('filterValues', filterValues);
-  console.log('selectedFilters', selectedFilters);
   return (
     <>
       <TitleComponent
