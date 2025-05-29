63ce60276b54af579daed101a49ef249a708d075
Alessio Bilello <alessio.bilello@emeal.nttdata.com>
2025-05-29 12:05:10 +0200

    add API getDebtPositionTypeOrgOperators and useDebtPositionTypeOrgSearch

--- src/routes/DebtTypeDetailView/DebtTypeDetailView.tsx
+++ src/routes/DebtTypeDetailView/DebtTypeDetailView.tsx
@@ -6,7 +6,8 @@ import { useNavigate, useParams } from 'react-router-dom';
 import { DetailAccordion } from '../../components/DetailAccordion/DetailAccordion';
 import {
   AccordionSectionConfig,
-  getAccordionSectionsConfig
+  getAccordionSectionsConfig,
+  OperatorsData
 } from '../../models/DebtTypeSectionsConfig';
 import { useEffect, useState } from 'react';
 import { STATE } from '../../store/types';
@@ -17,6 +18,8 @@ import { PageRoutes } from '../../App';
 import GenericDialog from '../../components/GenericDialog/GenericDialog';
 import { AxiosError, isAxiosError } from 'axios';
 import { useStore } from '../../store/GlobalStore';
+import { getDebtPositionTypeOrgOperators } from '../../api/debtPositionTypeOrgOperators';
+import { useDebtPositionTypeOrgSearch } from '../../api/debtTypesCreated';
 
 export const DebtTypeDetailView = () => {
   const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
@@ -63,19 +66,56 @@ export const DebtTypeDetailView = () => {
     console.error('debtPositionTypeOrgId is not a number');
   }
 
-  const { data, isLoading, isError, isSuccess, error } =
-    getDebtPositionTypeOrgById({
-      organizationId,
-      debtPositionTypeOrgId: Number(debtPositionTypeOrgId)
-    });
+  const {
+    data,
+    isLoading,
+    isError: isDebtTypeError,
+    isSuccess,
+    error: debtTypeError
+  } = getDebtPositionTypeOrgById({
+    organizationId,
+    debtPositionTypeOrgId: Number(debtPositionTypeOrgId)
+  });
+
+  const {
+    data: operatorsData,
+    isError: isOperatorsError,
+    error: operatorsError
+  } = getDebtPositionTypeOrgOperators(organizationId, {
+    debtPositionTypeOrgId: Number(debtPositionTypeOrgId)
+  });
+
+  const {
+    data: operatorsEnabledData,
+    mutate,
+    isError: isOperatorsEnabledError,
+    error: operatorsEnabledError
+  } = useDebtPositionTypeOrgSearch();
+
+  const buildOperatorsData = (): OperatorsData | null => {
+    if (!operatorsData || !operatorsEnabledData?.content?.[0]) {
+      return null;
+    }
+
+    const totalOperators = operatorsData.totalElements;
+    const enabledOperators =
+      operatorsEnabledData.content[0].enabledOperators || 0;
+
+    return {
+      totalOperators,
+      enabledOperators
+    };
+  };
 
   useEffect(() => {
     if (isSuccess && data) {
-      const sections = getAccordionSectionsConfig(data, t) || [];
+      const operatorsInfo = buildOperatorsData();
+      const sections = getAccordionSectionsConfig(data, operatorsInfo, t) || [];
       setAccordionSections(sections);
     }
-    if (isError && error) {
-      const axiosError = error as AxiosError;
+
+    if (isDebtTypeError && debtTypeError) {
+      const axiosError = debtTypeError as AxiosError;
       const isServerError =
         axiosError?.response?.status && axiosError.response.status >= 500;
 
@@ -83,7 +123,39 @@ export const DebtTypeDetailView = () => {
         utils.notify.emit(t('errors.fetchDebtPositionsTypes'), 'error');
       }
     }
-  }, [data, isLoading, isError, isSuccess, error]);
+
+    if (isOperatorsError && operatorsError) {
+      utils.notify.emit(t('errors.fetchOperators'), 'error');
+    }
+
+    if (isOperatorsEnabledError && operatorsEnabledError) {
+      utils.notify.emit(t('errors.fetchOperatorsEnabled'), 'error');
+    }
+  }, [
+    data,
+    isLoading,
+    isSuccess,
+    operatorsData,
+    operatorsEnabledData,
+    isDebtTypeError,
+    debtTypeError,
+    isOperatorsError,
+    operatorsError,
+    isOperatorsEnabledError,
+    operatorsEnabledError
+  ]);
+
+  useEffect(() => {
+    if (!isLoading && isSuccess && data?.code && !operatorsEnabledData) {
+      mutate({
+        organizationId,
+        filters: {
+          code: data.code,
+          description: data.description
+        }
+      });
+    }
+  }, [isLoading, isSuccess, data, operatorsEnabledData, mutate]);
 
   const actionButtons = [
     {
