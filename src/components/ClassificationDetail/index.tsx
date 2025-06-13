7744e0491da0d27cd3df6304969447d750ad2525
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-06-13 13:15:31 +0200

    [P4ADEV-2749] styiling elements

--- src/components/ClassificationDetail/index.tsx
+++ src/components/ClassificationDetail/index.tsx
@@ -1,7 +1,7 @@
 import { useStore } from '../../store/GlobalStore';
 import { useParams } from 'react-router';
 import { getClassificationDetails } from '../../api/getClassificationsDetail';
-import { Stack, Tab } from '@mui/material';
+import { Stack, Tab, useTheme } from '@mui/material';
 import { SyntheticEvent, useState } from 'react';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import DetailContainer, {
@@ -10,6 +10,7 @@ import DetailContainer, {
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 import { useTranslation } from 'react-i18next';
 import { formatDateTime } from '../../utils/formatters';
+import TitleComponent from '../TitleComponent/TitleComponent';
 
 export const ClassificationDetails = () => {
   const store = useStore();
@@ -21,107 +22,101 @@ export const ClassificationDetails = () => {
     Number(classificationId)
   );
   const [tabIndex, setTabIndex] = useState(0);
+  const theme = useTheme();
   const handleChange = (_event: SyntheticEvent, value: number) => {
-    console.log(value);
     setTabIndex(value);
   };
 
+  const targetTransalationDebtTypeData =
+    'classifications.detail.sections.telematicReceipt';
   const debtTypeData: Array<DetailData> = [
     {
-      label: t('classifications.detail.sections.telematicReceipt.debtType'),
+      label: t(`${targetTransalationDebtTypeData}.debtType`),
       value: 'test'
     },
     {
-      label: t(
-        'classifications.detail.sections.telematicReceipt.paymentObject'
-      ),
+      label: t(`${targetTransalationDebtTypeData}.paymentObject`),
       value: data?.remittanceDescription || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.amount'),
+      label: t(`${targetTransalationDebtTypeData}.amount`),
       value: data?.receiptPaymentAmount || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.dataPayment'),
+      label: t(`${targetTransalationDebtTypeData}.dataPayment`),
       value: formatDateTime(data?.receiptPaymentDateTime)
     },
     {
-      label: t(
-        'classifications.detail.sections.telematicReceipt.anagraficaAttestante'
-      ),
+      label: t(`${targetTransalationDebtTypeData}.anagraficaAttestante`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.CFAttestante'),
+      label: t(`${targetTransalationDebtTypeData}.CFAttestante`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.IUV'),
+      label: t(`${targetTransalationDebtTypeData}.IUV`),
       value: data?.iuv || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.IUD'),
+      label: t(`${targetTransalationDebtTypeData}.IUD`),
       value: data?.iud || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.IUR'),
+      label: t(`${targetTransalationDebtTypeData}.IUR`),
       value: data?.iur || ''
     },
     {
-      label: t(
-        'classifications.detail.sections.telematicReceipt.anagraficaPagatore'
-      ),
+      label: t(`${targetTransalationDebtTypeData}.anagraficaPagatore`),
       value: data?.receiptPayer?.fullName || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.CFPagatore'),
+      label: t(`${targetTransalationDebtTypeData}.CFPagatore`),
       value: data?.receiptPayer?.fiscalCode || ''
     },
     {
-      label: t(
-        'classifications.detail.sections.telematicReceipt.anagraficaVersante'
-      ),
+      label: t(`${targetTransalationDebtTypeData}.anagraficaVersante`),
       value: data?.receiptDebtor?.fullName || ''
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.CFVersante'),
+      label: t(`${targetTransalationDebtTypeData}.CFVersante`),
       value: data?.receiptDebtor?.fiscalCode || ''
     }
   ];
 
+  const targetTransalationNotifiedPayment =
+    'classifications.detail.sections.notifiedPayment';
   const notifiedPayment: Array<DetailData> = [
     {
-      label: t('classifications.detail.sections.telematicReceipt.debtType'),
+      label: t(`${targetTransalationNotifiedPayment}.debtType`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.notifiedPayment.paymentObject'),
+      label: t(`classifications.detail.sections.notifiedPayment.paymentObject`),
       value: data?.remittanceDescription || ''
     },
     {
-      label: t('classifications.detail.sections.notifiedPayment.amount'),
+      label: t(`classifications.detail.sections.notifiedPayment.amount`),
       value: data?.receiptPaymentAmount || ''
     },
     {
-      label: t(
-        'classifications.detail.sections.notifiedPayment.anagraficaPagatore'
-      ),
+      label: t(`${targetTransalationNotifiedPayment}.anagraficaPagatore`),
       value: formatDateTime(data?.receiptPaymentDateTime)
     },
     {
-      label: t('classifications.detail.sections.notifiedPayment.CFPagatore'),
+      label: t(`${targetTransalationNotifiedPayment}.CFPagatore`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.notifiedPayment.esecutionDate'),
+      label: t(`${targetTransalationNotifiedPayment}.esecutionDate`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.otherdata'),
+      label: t(`${targetTransalationNotifiedPayment}.otherdata`),
       value: 'test'
     },
     {
-      label: t('classifications.detail.sections.telematicReceipt.IUD'),
+      label: t(`${targetTransalationNotifiedPayment}.IUD`),
       value: data?.iud || ''
     }
   ];
@@ -154,67 +149,78 @@ export const ClassificationDetails = () => {
   const targetTransalationCassa = 'classifications.detail.sections.cassa';
   const cassa: Array<DetailData> = [
     {
-      label: t(`${targetTransalationCassa}.idRendicontazione`),
+      label: t(`${targetTransalationCassa}.conto`),
       value: 'test'
     },
     {
-      label: t(`${targetTransalationCassa}.data`),
+      label: t(`${targetTransalationCassa}.ordinante`),
       value: data?.remittanceDescription || ''
     },
     {
-      label: t(`${targetTransalationCassa}.idRegolamento`),
+      label: t(`${targetTransalationCassa}.codiceDocumento`),
       value: data?.receiptPaymentAmount || ''
     },
     {
-      label: t(`${targetTransalationCassa}.dataRegolamento`),
+      label: t(`${targetTransalationCassa}.dataValuta`),
       value: data?.receiptPaymentAmount || ''
     },
     {
-      label: t(`${targetTransalationCassa}.importoTotale`),
+      label: t(`${targetTransalationCassa}.annoBolletta`),
+      value: formatDateTime(data?.receiptPaymentDateTime)
+    },
+    {
+      label: t(`${targetTransalationCassa}.annoProvvisorio`),
+      value: data?.receiptPaymentAmount || ''
+    },
+    {
+      label: t(`${targetTransalationCassa}.dataContabile`),
+      value: data?.receiptPaymentAmount || ''
+    },
+    {
+      label: t(`${targetTransalationCassa}.codiceBolletta`),
       value: formatDateTime(data?.receiptPaymentDateTime)
     }
   ];
 
   return (
     <>
-      <TabContext value={tabIndex}>
+      <TitleComponent title={t('commons.routes.CLASSIFICATIONS')} />
+      <TabContext value={tabIndex} data-test-id="ClassificationDetailTabs">
         <TabList
           onChange={handleChange}
           aria-label="classification detail tabs"
           centered
           variant="fullWidth"
+          sx={{ backgroundColor: theme.palette.background.paper }}
         >
+          <Tab label={t(`${targetTransalationDebtTypeData}.title`)} value={0} />
           <Tab
-            label={t('classifications.detail.sections.telematicReceipt.title')}
-            value={0}
-          />
-          <Tab
-            label={t('classifications.detail.sections.rendicontazione.title')}
+            label={t(`${targetTransalationRendicontazione}.title`)}
             value={1}
           />
-          <Tab
-            label={t('classifications.detail.sections.cassa.title')}
-            value={2}
-          />
+          <Tab label={t(`${targetTransalationCassa}.title`)} value={2} />
         </TabList>
-        <TabPanel value={0} sx={{ padding: 0 }}>
+        <TabPanel
+          value={0}
+          sx={{ padding: 0 }}
+          data-test-id="ClassificationDetailTabNotifiedPayment"
+        >
           <Stack spacing={3}>
             <DetailContainer
               sections={[
                 {
                   inline: true,
                   title: {
-                    label: t(
-                      'classifications.detail.sections.telematicReceipt.title'
-                    ),
-                    uppercase: true
+                    textTransform: 'uppercase',
+                    fontWeight: 700,
+                    fontSize: '14px',
+                    label: t(`${targetTransalationDebtTypeData}.title`)
                   },
                   data: debtTypeData,
                   footerLink: {
-                    label: t(
-                      'classifications.detail.sections.telematicReceipt.link'
-                    ),
-                    icon: <ArrowRightAltIcon />
+                    label: t(`${targetTransalationDebtTypeData}.link`),
+                    icon: <ArrowRightAltIcon />,
+                    iconPosition: 'right'
                   }
                 }
               ]}
@@ -224,10 +230,10 @@ export const ClassificationDetails = () => {
                 {
                   inline: true,
                   title: {
-                    label: t(
-                      'classifications.detail.sections.notifiedPayment.title'
-                    ),
-                    uppercase: true
+                    textTransform: 'uppercase',
+                    fontWeight: 700,
+                    fontSize: '14px',
+                    label: t(`${targetTransalationNotifiedPayment}.title`)
                   },
                   data: notifiedPayment
                 }
@@ -235,41 +241,51 @@ export const ClassificationDetails = () => {
             />
           </Stack>
         </TabPanel>
-        <TabPanel value={1} sx={{ padding: 0 }}>
+        <TabPanel
+          value={1}
+          sx={{ padding: 0 }}
+          data-test-id="ClassificationDetailTabNotifiedRendicontazione"
+        >
           <DetailContainer
             sections={[
               {
                 inline: true,
                 title: {
-                  label: t(
-                    'classifications.detail.sections.rendicontazione.title'
-                  ),
-                  uppercase: true
+                  label: t(`${targetTransalationRendicontazione}.title`),
+                  textTransform: 'uppercase',
+                  fontWeight: 700,
+                  fontSize: '14px'
                 },
                 data: rendicontazione,
                 footerLink: {
-                  label: t(
-                    'classifications.detail.sections.rendicontazione.link'
-                  ),
-                  icon: <ArrowRightAltIcon />
+                  label: t(`${targetTransalationRendicontazione}.link`),
+                  icon: <ArrowRightAltIcon />,
+                  iconPosition: 'right'
                 }
               }
             ]}
           />
         </TabPanel>
-        <TabPanel value={2} sx={{ padding: 0 }}>
+        <TabPanel
+          value={2}
+          sx={{ padding: 0 }}
+          data-test-id="ClassificationDetailTabCassa"
+        >
           <DetailContainer
             sections={[
               {
                 inline: true,
                 title: {
-                  label: t('classifications.detail.sections.cassa.title'),
-                  uppercase: true
+                  label: t(`${targetTransalationCassa}.title`),
+                  fontWeight: 700,
+                  fontSize: '14px',
+                  textTransform: 'uppercase'
                 },
                 data: cassa,
                 footerLink: {
-                  label: t('classifications.detail.sections.cassa.link'),
-                  icon: <ArrowRightAltIcon />
+                  label: t(`${targetTransalationCassa}.link`),
+                  icon: <ArrowRightAltIcon />,
+                  iconPosition: 'right'
                 }
               }
             ]}
--- src/components/DetailContainer/DetailContainer.tsx
+++ src/components/DetailContainer/DetailContainer.tsx
@@ -7,12 +7,13 @@ import {
   Chip,
   ChipOwnProps,
   Button,
-  Divider
+  Divider,
+  TypographyOwnProps
 } from '@mui/material';
-import { Variant } from '@mui/material/styles/createTypography';
 import { useTranslation } from 'react-i18next';
 import { moneyFormat } from '../../utils/formatters';
 import React from 'react';
+
 export type DetailData = {
   label: string;
   value: string | number;
@@ -23,15 +24,16 @@ export type DetailData = {
   };
   childrenComponent?: React.ReactNode;
 };
+
 export type titleConfig = {
-  label: string;
-  variant?: Variant;
-  uppercase?: boolean;
-};
+  label?: string;
+} & TypographyOwnProps;
+
 export type footerLinkConfig = {
   label: string;
   icon?: React.ReactNode;
   onLinkClick?: () => void;
+  iconPosition?: 'left' | 'right';
 };
 
 export type DetailSectionProps = {
@@ -55,7 +57,6 @@ const DetailContainer = ({
 }: DetailSectionProps) => {
   const theme = useTheme();
   const { t } = useTranslation();
-
   const renderItemValue = (item: DetailData): JSX.Element => {
     if (item.label === t('commons.state')) {
       return (
@@ -104,78 +105,89 @@ const DetailContainer = ({
     <Card sx={{ borderRadius: 2, height: 'auto' }}>
       <CardContent>
         <Grid container spacing={2}>
-          {sections.map((section, index) => (
-            <Grid
-              item
-              xs={12}
-              md={fullWidthSections || sections.length === 1 ? 12 : 6}
-              key={index}
-            >
-              {section.title ? (
-                <Typography variant={section.title.variant}>
-                  {section.title.uppercase
-                    ? section.title.label.toUpperCase()
-                    : section.title.label}
-                </Typography>
-              ) : null}
-              <Grid container direction="column">
-                {section.data.map((item, index) => (
-                  <Grid
-                    container
-                    spacing={1}
-                    marginTop={1}
-                    key={index}
-                    direction={section.inline ? 'row' : 'column'}
-                  >
+          {sections.map((section, index) => {
+            const iconPosition = section.footerLink?.iconPosition || 'left';
+            return (
+              <Grid
+                item
+                xs={12}
+                md={fullWidthSections || sections.length === 1 ? 12 : 6}
+                key={index}
+              >
+                {section.title ? (
+                  <Typography {...section.title}>
+                    {section.title.label}
+                  </Typography>
+                ) : null}
+                <Grid container direction="column">
+                  {section.data.map((item, index) => (
                     <Grid
-                      item
-                      lg={section.inline ? 6 : 12}
-                      md={section.inline ? 6 : 12}
+                      container
+                      spacing={1}
+                      marginTop={1}
+                      key={index}
+                      direction={section.inline ? 'row' : 'column'}
                     >
-                      <Typography
-                        variant="body2"
-                        color={theme.palette.action.active}
+                      <Grid
+                        item
+                        lg={section.inline ? 6 : 12}
+                        md={section.inline ? 6 : 12}
                       >
-                        {item.label}
-                      </Typography>
+                        <Typography
+                          variant="body2"
+                          color={theme.palette.action.active}
+                        >
+                          {item.label}
+                        </Typography>
+                      </Grid>
+                      <Grid
+                        item
+                        lg={section.inline ? 6 : 12}
+                        md={section.inline ? 6 : 12}
+                      >
+                        {renderItemValue(item)}
+                      </Grid>
+                      {section.divider && index !== section.data.length - 1 && (
+                        <Divider
+                          orientation="horizontal"
+                          flexItem
+                          sx={{ display: 'block', mt: 1 }}
+                        />
+                      )}
                     </Grid>
-                    <Grid
-                      item
-                      lg={section.inline ? 6 : 12}
-                      md={section.inline ? 6 : 12}
-                    >
-                      {renderItemValue(item)}
+                  ))}
+                  {section.footerLink && (
+                    <Grid item xs={12} sx={{ mt: 3, textAlign: 'left' }}>
+                      <Button
+                        size="small"
+                        startIcon={
+                          section.footerLink.icon && iconPosition === 'left'
+                            ? section.footerLink.icon
+                            : undefined
+                        }
+                        endIcon={
+                          section.footerLink.icon && iconPosition === 'right'
+                            ? section.footerLink.icon
+                            : undefined
+                        }
+                        variant="text"
+                        fullWidth={false}
+                        onClick={section.footerLink.onLinkClick}
+                        sx={{
+                          justifyContent: 'flex-start',
+                          paddingLeft: 0,
+                          fontWeight: 700,
+                          fontSize: 16
+                        }}
+                      >
+                        {section.footerLink.label}
+                      </Button>
                     </Grid>
-                    {section.divider && index !== section.data.length - 1 && (
-                      <Divider
-                        orientation="horizontal"
-                        flexItem
-                        sx={{ display: 'block', mt: 1 }}
-                      />
-                    )}
-                  </Grid>
-                ))}
-                {section.footerLink && (
-                  <Grid item xs={12} sx={{ mt: 3, textAlign: 'left' }}>
-                    <Button
-                      size="small"
-                      startIcon={
-                        section.footerLink.icon
-                          ? section.footerLink.icon
-                          : undefined
-                      }
-                      variant="text"
-                      fullWidth={false}
-                      onClick={section.footerLink.onLinkClick}
-                      sx={{ justifyContent: 'flex-start', paddingLeft: 0 }}
-                    >
-                      {section.footerLink.label}
-                    </Button>
-                  </Grid>
-                )}
+                  )}
+                </Grid>
               </Grid>
-            </Grid>
-          ))}
+            );
+          })}
         </Grid>
       </CardContent>
     </Card>
