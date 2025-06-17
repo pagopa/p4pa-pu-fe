eb8c2124b1d380e76cbef752c23727e475184647
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2025-06-17 09:32:16 +0200

    feat: [P4ADEV-2749] Classification detail page (#248)

--- src/components/DetailContainer/DetailContainer.tsx
+++ src/components/DetailContainer/DetailContainer.tsx
@@ -7,15 +7,21 @@ import {
   Chip,
   ChipOwnProps,
   Button,
-  Divider
+  Divider,
+  TypographyOwnProps
 } from '@mui/material';
-import { Variant } from '@mui/material/styles/createTypography';
 import { useTranslation } from 'react-i18next';
-import { moneyFormat } from '../../utils/formatters';
+import {
+  formatDate,
+  formatDateTime,
+  moneyFormat
+} from '../../utils/formatters';
 import React from 'react';
+
 export type DetailData = {
   label: string;
-  value: string | number;
+  value?: string | number;
+  valueType?: 'amount' | 'date' | 'dateTime' | 'status';
   variant?: 'body1' | 'body2' | 'h6' | 'subtitle1' | 'monospaced';
   chipConfig?: {
     color?: ChipOwnProps['color'];
@@ -23,15 +29,16 @@ export type DetailData = {
   };
   childrenComponent?: React.ReactNode;
 };
+
 export type titleConfig = {
-  label: string;
-  variant?: Variant;
-  uppercase?: boolean;
-};
+  label?: string;
+} & TypographyOwnProps;
+
 export type footerLinkConfig = {
   label: string;
   icon?: React.ReactNode;
   onLinkClick?: () => void;
+  iconPosition?: 'left' | 'right';
 };
 
 export type DetailSectionProps = {
@@ -45,7 +52,9 @@ export type DetailSectionProps = {
   fullWidthSections?: boolean;
 };
 
-const stateColors: Record<DetailData['value'], ChipOwnProps['color']> = {
+type Status = 'Pagato';
+
+const stateColors: Record<Status, ChipOwnProps['color']> = {
   Pagato: 'success'
 };
 
@@ -55,37 +64,21 @@ const DetailContainer = ({
 }: DetailSectionProps) => {
   const theme = useTheme();
   const { t } = useTranslation();
-
   const renderItemValue = (item: DetailData): JSX.Element => {
-    if (item.label === t('commons.state')) {
+    if (item.childrenComponent) {
+      return <>{item.childrenComponent}</>;
+    }
+
+    if (item.valueType === 'status' && item.value) {
       return (
         <React.Fragment>
           <Chip
-            color={item.chipConfig?.color ?? stateColors[item.value]}
+            color={item.chipConfig?.color ?? stateColors[item.value as Status]}
             label={t(`commons.status.${item.value}`)}
             variant={item.chipConfig?.variant}
           />
         </React.Fragment>
       );
-    }
-
-    if (item.label === t('commons.amount')) {
-      return (
-        <React.Fragment>
-          <Typography
-            fontWeight={item.variant ?? 600}
-            variant={item.variant ?? 'body1'}
-          >
-            {moneyFormat(
-              typeof item.value === 'number' ? item.value : Number(item.value)
-            )}
-          </Typography>
-        </React.Fragment>
-      );
-    }
-
-    if (item.childrenComponent) {
-      return <>{item.childrenComponent}</>;
     }
 
     return (
@@ -94,7 +87,25 @@ const DetailContainer = ({
           fontWeight={item.variant ?? 600}
           variant={item.variant ?? 'body1'}
         >
-          {item.value || '-'}
+          {
+            // eslint-disable-next-line sonarjs/function-return-type
+            (() => {
+              if (item.valueType === 'amount') {
+                return moneyFormat(
+                  typeof item.value === 'number'
+                    ? item.value
+                    : Number(item.value)
+                );
+              }
+              if (item.valueType === 'date') {
+                return formatDate(`${item.value}`);
+              }
+              if (item.valueType === 'dateTime') {
+                return formatDateTime(`${item.value}`);
+              }
+              return item.value || '-';
+            })()
+          }
         </Typography>
       </React.Fragment>
     );
@@ -104,78 +115,89 @@ const DetailContainer = ({
     <Card sx={{ borderRadius: 2, height: 'auto' }}>
       <CardContent>
         <Grid container spacing={2}>
-          {sections.map((section, index) => (
-            <Grid
-              item
-              xs={12}
-              md={fullWidthSections || sections.length === 1 ? 12 : 6}
-              key={index}
-            >
-              {section.title ? (
-                <Typography variant={section.title.variant}>
-                  {section.title.uppercase
-                    ? section.title.label.toUpperCase()
-                    : section.title.label}
-                </Typography>
-              ) : null}
-              <Grid container direction="column">
-                {section.data.map((item, index) => (
-                  <Grid
-                    container
-                    spacing={1}
-                    marginTop={1}
-                    key={index}
-                    direction={section.inline ? 'row' : 'column'}
-                  >
+          {sections.map((section, index) => {
+            const iconPosition = section.footerLink?.iconPosition || 'left';
+            return (
+              <Grid
+                item
+                xs={12}
+                md={fullWidthSections || sections.length === 1 ? 12 : 6}
+                key={index}
+              >
+                {section.title ? (
+                  <Typography {...section.title}>
+                    {section.title.label}
+                  </Typography>
+                ) : null}
+                <Grid container direction="column">
+                  {section.data.map((item, index) => (
                     <Grid
-                      item
-                      lg={section.inline ? 6 : 12}
-                      md={section.inline ? 6 : 12}
+                      container
+                      spacing={1}
+                      marginTop={1}
+                      key={index}
+                      direction={section.inline ? 'row' : 'column'}
                     >
-                      <Typography
-                        variant="body2"
-                        color={theme.palette.action.active}
+                      <Grid
+                        item
+                        lg={section.inline ? 6 : 12}
+                        md={section.inline ? 6 : 12}
                       >
-                        {item.label}
-                      </Typography>
+                        <Typography
+                          variant="body2"
+                          color={theme.palette.action.active}
+                        >
+                          {item.label}
+                        </Typography>
+                      </Grid>
+                      <Grid
+                        item
+                        lg={section.inline ? 6 : 12}
+                        md={section.inline ? 6 : 12}
+                      >
+                        {renderItemValue(item)}
+                      </Grid>
+                      {section.divider && index !== section.data.length - 1 && (
+                        <Divider
+                          orientation="horizontal"
+                          flexItem
+                          sx={{ display: 'block', mt: 1 }}
+                        />
+                      )}
                     </Grid>
-                    <Grid
-                      item
-                      lg={section.inline ? 6 : 12}
-                      md={section.inline ? 6 : 12}
-                    >
-                      {renderItemValue(item)}
+                  ))}
+                  {section.footerLink && (
+                    <Grid item xs={12} sx={{ mt: 3, textAlign: 'left' }}>
+                      <Button
+                        size="small"
+                        startIcon={
+                          section.footerLink.icon && iconPosition === 'left'
+                            ? section.footerLink.icon
+                            : undefined
+                        }
+                        endIcon={
+                          section.footerLink.icon && iconPosition === 'right'
+                            ? section.footerLink.icon
+                            : undefined
+                        }
+                        variant="text"
+                        fullWidth={false}
+                        onClick={section.footerLink.onLinkClick}
+                        sx={{
+                          justifyContent: 'flex-start',
+                          paddingLeft: 0,
+                          fontWeight: 700,
+                          fontSize: 16
+                        }}
+                      >
+                        {section.footerLink.label}
+                      </Button>
                     </Grid>
-                    {section.divider && index !== section.data.length - 1 && (
-                      <Divider
-                        orientation="horizontal"
-                        flexItem
-                        sx={{ display: 'block', mt: 1 }}
-                      />
-                    )}
-                  </Grid>
-                ))}
-                {section.footerLink && (
-                  <Grid item xs={12} sx={{ mt: 3, textAlign: 'left' }}>
-                    <Button
-                      size="small"
-                      startIcon={
-                        section.footerLink.icon
-                          ? section.footerLink.icon
-                          : undefined
-                      }
-                      variant="text"
-                      fullWidth={false}
-                      onClick={section.footerLink.onLinkClick}
-                      sx={{ justifyContent: 'flex-start', paddingLeft: 0 }}
-                    >
-                      {section.footerLink.label}
-                    </Button>
-                  </Grid>
-                )}
+                  )}
+                </Grid>
               </Grid>
-            </Grid>
-          ))}
+            );
+          })}
         </Grid>
       </CardContent>
     </Card>
