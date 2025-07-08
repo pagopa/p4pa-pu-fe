4fe7da49e87c3f9bae7c5b5c1694ffa3458e4d56
luigilesca <luigi.lesca@nttdata.com>
2025-07-08 17:45:52 +0200

    create new getAssessmentDetail api

---
+++ src/api/assessmentDetail.ts
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
+import utils from '../utils';
+import { parseAndLog } from '../utils/loaders';
+import { pagedAssessmentsRowsDetailSchema } from '../../generated/zod-schema';
+
+/**
+ * Hook for getting the details of a specific assessment
+ * @param organizationId - Organization ID
+ * @param assessmentId - Assessment ID
+ * @param options - Additional options for the query
+ * @returns useQuery hook for executing the API call
+ */
+export const getAssessmentDetail = (
+  organizationId: number,
+  assessmentId: number,
+  filters?: Record<string, unknown>,
+  options: Record<string, unknown> = {}
+) => {
+  return useQuery({
+    queryKey: ['getAssessmentDetail', organizationId, assessmentId, filters],
+    queryFn: async () => {
+      const { data: assessmentDetail } =
+        await utils.apiClient.bff.getPagedAssessmentsDetails(
+          organizationId,
+          assessmentId,
+          filters
+        );
+
+      if (assessmentDetail) {
+        parseAndLog(pagedAssessmentsRowsDetailSchema, assessmentDetail);
+      }
+
+      return assessmentDetail;
+    },
+    enabled: !!organizationId && !!assessmentId,
+    ...options
+  });
+};
