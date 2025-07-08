fcd6dc4699b4c109b60f14ee471cf133f7892e06
luigilesca <luigi.lesca@nttdata.com>
2025-07-08 17:47:05 +0200

    linn new route assessment detail and add translations

--- src/routes/assessment.tsx
+++ src/routes/assessment.tsx
@@ -1,6 +1,7 @@
 import { RouteHandleObject } from '../models/Routes';
 import Assessment from '../components/Assessment';
 import AssessmentSearchResults from './AssessmentSearchResults';
+import AssessmentDetail from './AssessmentDetail/AssessmentDetail';
 
 export const assessmentRoutes = [
   {
@@ -20,6 +21,15 @@ export const assessmentRoutes = [
         id: 'ASSESSMENT_SEARCH_RESULTS',
         path: 'search-results',
         element: <AssessmentSearchResults />,
+        handle: {
+          backButton: true,
+          hideBreadcrumbs: false
+        } as RouteHandleObject
+      },
+      {
+        id: 'ASSESSMENT_DETAIL',
+        path: 'detail/:id',
+        element: <AssessmentDetail />,
         handle: {
           backButton: true,
           hideBreadcrumbs: false
