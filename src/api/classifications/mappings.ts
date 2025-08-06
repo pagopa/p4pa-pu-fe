11d47f779ddb5309c36c8265b0950b0331b5a1a7
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-08-06 17:15:38 +0200

    [P4ADEV-3618] installments and classifications api input params type fix

--- src/api/classifications/mappings.ts
+++ src/api/classifications/mappings.ts
@@ -1,6 +1,7 @@
-import { format } from 'date-fns/format';
+import { formatISO } from 'date-fns';
 import { ClassificationsEnum } from '../../../generated/data-contracts';
 import { FilterValues } from '../../models/Filters';
+import utils from '../../utils';
 
 export type ClassificationsFilteredRequest = {
   filters: FilterValues;
@@ -8,76 +9,61 @@ export type ClassificationsFilteredRequest = {
   sort: Array<string>;
 };
 
+type ClassificationsQueryParams = Parameters<
+  typeof utils.apiClient.bff.getTreasuredClassifications
+>[1];
+
+/** this is a tempory function that will be removed with the task P4ADEV-3617 */
+const temporyFormatDateTimeFunction = (date?: Date | null) => {
+  if (date === null || !date) return;
+  return formatISO(date);
+};
+
 export const buildQueryParams = ({
   filters,
   pagination,
   sort
-}: ClassificationsFilteredRequest) => ({
-  ...(filters.CLASSIFICATION_TYPE && {
-    label: filters.CLASSIFICATION_TYPE as ClassificationsEnum
-  }),
-  ...(filters.IUV && { iuv: filters.IUV }),
-  ...(filters.IUR && { iur: filters.IUR }),
-  ...(filters.IUD && { iud: filters.IUD }),
-  ...(filters.IUF && { iuf: filters.IUF }),
-  ...(filters.LAST_CLASSIFICATION_DATE_FROM && {
-    lastClassificationDateFrom: format(
-      filters.LAST_CLASSIFICATION_DATE_FROM,
-      'yyyy-MM-dd'
-    )
-  }),
-  ...(filters.LAST_CLASSIFICATION_DATE_TO && {
-    lastClassificationDateTo: format(
-      filters.LAST_CLASSIFICATION_DATE_TO,
-      'yyyy-MM-dd'
-    )
-  }),
-  ...(filters.REGULATION_DATE_FROM && {
-    regulationDateFrom: format(filters.REGULATION_DATE_FROM, 'yyyy-MM-dd')
-  }),
-  ...(filters.REGULATION_DATE_TO && {
-    regulationDateTo: format(filters.REGULATION_DATE_TO, 'yyyy-MM-dd')
-  }),
-  ...(filters.AMOUNT && {
-    billAmountCents: filters.AMOUNT * 100
-  }),
-  ...(filters.BILL_DATE_FROM && {
-    billDateFrom: format(filters.BILL_DATE_FROM, 'yyyy-MM-dd')
-  }),
-  ...(filters.BILL_DATE_TO && {
-    billDateTo: format(filters.BILL_DATE_TO, 'yyyy-MM-dd')
-  }),
-  ...(filters.PAYMENT_DATE_FROM && {
-    paymentDateTimeFrom: filters.PAYMENT_DATE_FROM.toISOString()
-  }),
-  ...(filters.PAYMENT_DATE_TO && {
-    paymentDateTimeTo: filters.PAYMENT_DATE_TO.toISOString()
-  }),
-  ...(filters.PAY_DATE_FROM && {
-    payDateFrom: format(filters.PAY_DATE_FROM, 'yyyy-MM-dd')
-  }),
-  ...(filters.PAY_DATE_TO && {
-    payDateTo: format(filters.PAY_DATE_TO, 'yyyy-MM-dd')
-  }),
-  ...(filters.REGULATION_UNIQUE_IDENTIFIER && {
-    regulationUniqueIdentifier: filters.REGULATION_UNIQUE_IDENTIFIER
-  }),
-  ...(filters.ACCOUNT_REGISTRY_CODE && {
-    accountRegistryCode: filters.ACCOUNT_REGISTRY_CODE
-  }),
-  ...(filters.REMITTANCE_INFORMATION && {
-    remittanceInformation: filters.REMITTANCE_INFORMATION
-  }),
-  ...(filters.PSP_COMPANY_NAME && {
-    pspCompanyName: filters.PSP_COMPANY_NAME
-  }),
-  ...(filters.REGION_VALUE_DATE_FROM && {
-    regionValueDateFrom: format(filters.REGION_VALUE_DATE_FROM, 'yyyy-MM-dd')
-  }),
-  ...(filters.REGION_VALUE_DATE_TO && {
-    regionValueDateTo: format(filters.REGION_VALUE_DATE_TO, 'yyyy-MM-dd')
-  }),
-  page: pagination.page,
-  size: pagination.size,
-  ...(sort.length && { sort })
-});
+}: ClassificationsFilteredRequest): ClassificationsQueryParams =>
+  Object.fromEntries(
+    Object.entries({
+      label: filters.CLASSIFICATION_TYPE as ClassificationsEnum,
+      iuv: filters.IUV,
+      iur: filters.IUR,
+      iud: filters.IUD,
+      iuf: filters.IUF,
+      lastClassificationDateTimeFrom: temporyFormatDateTimeFunction(
+        filters.LAST_CLASSIFICATION_DATE_FROM
+      ),
+      lastClassificationDateTimeTo: temporyFormatDateTimeFunction(
+        filters.LAST_CLASSIFICATION_DATE_TO
+      ),
+      regulationDateTimeFrom: temporyFormatDateTimeFunction(
+        filters.REGULATION_DATE_FROM
+      ),
+      regulationDateTimeTo: temporyFormatDateTimeFunction(
+        filters.REGULATION_DATE_TO
+      ),
+      billAmountCents: filters.AMOUNT ? filters.AMOUNT * 100 : undefined,
+      billDateTimeFrom: temporyFormatDateTimeFunction(filters.BILL_DATE_FROM),
+      billDateTimeTo: temporyFormatDateTimeFunction(filters.BILL_DATE_TO),
+      paymentDateTimeFrom: temporyFormatDateTimeFunction(
+        filters.PAYMENT_DATE_FROM
+      ),
+      paymentDateTimeTo: temporyFormatDateTimeFunction(filters.PAYMENT_DATE_TO),
+      payDateTimeFrom: temporyFormatDateTimeFunction(filters.PAY_DATE_FROM),
+      payDateTimeTo: temporyFormatDateTimeFunction(filters.PAY_DATE_TO),
+      regulationUniqueIdentifier: filters.REGULATION_UNIQUE_IDENTIFIER,
+      accountRegistryCode: filters.ACCOUNT_REGISTRY_CODE,
+      remittanceInformation: filters.REMITTANCE_INFORMATION,
+      pspCompanyName: filters.PSP_COMPANY_NAME,
+      regionValueDateTimeFrom: temporyFormatDateTimeFunction(
+        filters.REGION_VALUE_DATE_FROM
+      ),
+      regionValueDateTimeTo: temporyFormatDateTimeFunction(
+        filters.REGION_VALUE_DATE_TO
+      ),
+      page: pagination.page,
+      size: pagination.size,
+      sort
+    }).filter(([, v]) => v)
+  );
