61a763bc6f247648200912c1764bfa9a6ef264ec
Strato Ivan Di Luccio <34749257+stratoivandiluccio@users.noreply.github.com>
2025-08-28 18:14:34 +0200

    feat: [P4ADEV-3541] implement download functionality for ingestion flow files in ReportingDetail and ReportingDataGrid (#360)

--- src/api/getPagoPaRegistries/index.ts
+++ src/api/getPagoPaRegistries/index.ts
@@ -1,12 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-import utils from '../utils';
-import { parseAndLog } from '../utils/loaders';
-import { pagedPagoPaRegistrySchema } from '../../generated/zod-schema';
-import { FilteredRequest } from '../models/Filters';
-import {
-  getQueryFromFilterValues,
-  NodoFilterValues
-} from '../routes/Events/configs';
+import utils from '../../utils';
+import { parseAndLog } from '../../utils/loaders';
+import { pagedPagoPaRegistrySchema } from '../../../generated/zod-schema';
+import { FilteredRequest } from '../../models/Filters';
+import { NodoFilterValues } from '../../routes/Events/configs';
+import { buildQueryParams } from './mapping';
 
 const getPagoPaRegistries = (organizationId: number) =>
   useMutation({
@@ -16,11 +14,7 @@ const getPagoPaRegistries = (organizationId: number) =>
       pagination,
       sort
     }: FilteredRequest<NodoFilterValues>) => {
-      const query = {
-        ...getQueryFromFilterValues(filters),
-        ...pagination,
-        sort
-      };
+      const query = buildQueryParams({ filters, pagination, sort });
       const { data: response } = await utils.apiClient.bff.getPagoPaRegistries(
         organizationId,
         query
