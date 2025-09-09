ace3523f5aff325b5a04ea0dcf26211bed6d19c1
Ivan Di Luccio <ivan.diluccio@pagopa.it>
2025-09-09 12:55:46 +0200

    [P4ADEV-3612] remove unnecessary slice from selectedFilters mapping

--- src/components/MultiFilter/MultiFilter.tsx
+++ src/components/MultiFilter/MultiFilter.tsx
@@ -43,7 +43,6 @@ const MultiFilter = ({ filterMap, onFilterInteraction }: MultiFilterProps) => {
   return (
     <Stack gap={3}>
       {selectedFilters
-        .slice()
         .map((filterId, index) => (
           <Stack
             key={filterId}
