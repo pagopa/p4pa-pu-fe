74e90c5e319a6ab3663606db0238893038b1f586
Vito Falco <vitofalco@gmail.com>
2025-09-11 14:21:46 +0200

    fix: [P4ADEV-3649] Add chip to detail and list of installments (#398)

--- src/components/ChipTruncateTooltip/index.tsx
+++ src/components/ChipTruncateTooltip/index.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { Tooltip, Chip } from '@mui/material';
 import { ChipProps } from '@mui/material/Chip';
 
-const ChipTruncateTooltip: React.FC<ChipProps> = ({ label, color }) => {
+const ChipTruncateTooltip: React.FC<ChipProps> = ({
+  label,
+  color,
+  variant
+}) => {
   return (
     <Tooltip title={label} arrow>
       <Chip
         label={label}
         color={color}
         size="small"
+        variant={variant || 'filled'}
         sx={{
           '.MuiChip-label': {
             overflow: 'hidden',
             whiteSpace: 'nowrap',
             textOverflow: 'ellipsis',
-            display: 'block'
+            display: 'block',
+            cursor: 'default'
           }
         }}
       />
