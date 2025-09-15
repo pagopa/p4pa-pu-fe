85902f8c407c3980f19f6b688b87090f1a2e0428
Vito Falco <vitofalco@gmail.com>
2025-09-15 11:31:50 +0200

    Map request to debtypes catalog

--- src/api/debtPositionsTypes/mappings.ts
+++ src/api/debtPositionsTypes/mappings.ts
@@ -1,3 +1,6 @@
+import { DebtPositionTypeRequestBody } from '../../../generated/data-contracts';
+import { Step2Data } from '../../routes/DebtTypeCreate/components/Step2Settings';
+
 export type DebtPositionTypeWithCountFilters = {
   description?: string;
 };
@@ -15,8 +18,24 @@ export const buildQueryParams = ({
 }: DebtPositionTypeWithCountFilteredRequest) => ({
   page: pagination.page,
   size: pagination.size,
-  ...(filters?.description && {
-    description: filters.description
-  }),
-  ...(sort.length && { sort })
+  description: filters.description,
+  sort
+});
+
+export const buildQueryPostParams = (
+  step2Data: Step2Data
+): DebtPositionTypeRequestBody => ({
+  debtPositionTypeId: step2Data.debtPositionTypeId,
+  code: step2Data.code || '',
+  description: step2Data.description || '',
+  orgType: step2Data.orgType || '',
+  macroArea: step2Data.macroArea || '',
+  serviceType: step2Data.serviceType || '',
+  collectingReason: step2Data.collectingReason || '',
+  taxonomyCode: step2Data.taxonomyCode || '',
+  flagAnonymousFiscalCode: step2Data.flagAnonymousFiscalCode,
+  flagMandatoryDueDate: step2Data.flagMandatoryDueDate,
+  flagNotifyIo: step2Data.flagNotifyIo,
+  ioTemplateMessage: step2Data.ioTemplateMessage,
+  ioTemplateSubject: step2Data.ioTemplateSubject
 });
