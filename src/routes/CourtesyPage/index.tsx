de2733778e7f33f09425df8d5ecc500981458bdd
Michele Moio <michelemoiodev@gmail.com>
2025-10-07 16:10:11 +0200

    feat: [P4ADEV-3839] adds draft ec operator

--- src/routes/CourtesyPage/index.tsx
+++ src/routes/CourtesyPage/index.tsx
@@ -4,28 +4,43 @@ import ResponsePage from '../../components/ResponsePage/ResponsePage';
 import Stack from '@mui/material/Stack';
 import { useTheme } from '@mui/material/styles';
 import utils from '../../utils';
+import { operatorComputedRole } from '../../store/OperatorRoleStore';
+import { ExtendedOperatoRole } from '../../models/OperatorRole';
+import { WaitingIcon } from '../../assets/icons/waiting';
+
+const OperatorIcon = ({
+  operatorRole
+}: {
+  operatorRole: ExtendedOperatoRole;
+}) =>
+  operatorRole !== ExtendedOperatoRole.ROLE_OPER ? (
+    <AbacusIcon />
+  ) : (
+    <WaitingIcon />
+  );
 
 export const CourtesyPage = () => {
   const { t } = useTranslation();
   const { spacing } = useTheme();
+  const operatorRole = operatorComputedRole.value;
 
-  const toLogin = () => {
+  const toHome = () => {
     window.location.replace(utils.config.loginUrl);
   };
 
   return (
     <Stack maxWidth={spacing(45)} margin="auto">
       <ResponsePage
-        icon={<AbacusIcon />}
-        title={t('DraftCourtesyPage.superadmin.title')}
-        description={t('DraftCourtesyPage.superadmin.description')}
+        icon={<OperatorIcon operatorRole={operatorRole} />}
+        title={t(`DraftCourtesyPage.${operatorRole}.title`)}
+        description={t(`DraftCourtesyPage.${operatorRole}.description`)}
         buttonConfig={[
           {
-            variant: 'outlined',
+            variant: 'contained',
             size: 'large',
-            buttonLabel: t('commons.close'),
+            buttonLabel: t('commons.backToHome'),
             actionID: 'back',
-            onButtonClick: toLogin
+            onButtonClick: toHome
           }
         ]}
       />
