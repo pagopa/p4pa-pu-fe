5614b4abe414b48449faa5e10235baf76863c3cf
Vito Falco <vitofalco@gmail.com>
2025-10-08 16:21:08 +0200

    lintfix

--- src/routes/Organizations/OrganizationDetail.test.tsx
+++ src/routes/Organizations/OrganizationDetail.test.tsx
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { fireEvent, render, screen } from '../../__tests__/renderers';
+import { render, screen } from '../../__tests__/renderers';
 import { getOrganizationDetail } from '../../api/organizations';
 import OrganizationDetail from './OrganizationDetail';
 import utils from '../../utils';
