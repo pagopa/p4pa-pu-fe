50f37d83703e2e6000f6ec46c8ab8e7b01b8962c
Michele Moio <michelemoiodev@gmail.com>
2025-10-16 12:54:15 +0200

    feat: [P4ADEV-3948] aria announcement on route change (#465)

---
+++ src/components/RouteChangeAnnouncement/index.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useLocation } from 'react-router';
+
+export const RouteChangeAnnouncement = () => {
+  const location = useLocation();
+  const { t } = useTranslation();
+  const [message, setMessage] = useState('');
+
+  const updateMessage = () => {
+    const page = document?.title.replace(/ - .+$/, ''); // Remove trailing app name
+    const newMessage = t('a11y.navigation.announcement', {
+      page
+    });
+    setMessage(newMessage);
+  };
+
+  useEffect(() => {
+    // Clear message to reset live region
+    setMessage('');
+    const timeoutId = setTimeout(updateMessage, 200); // helps with screen readers
+    return () => clearTimeout(timeoutId);
+  }, [location]);
+
+  return (
+    <div
+      role="alert"
+      aria-live="polite"
+      style={{
+        position: 'absolute',
+        width: '1px',
+        height: '1px',
+        margin: '-1px',
+        border: '0',
+        padding: '0',
+        overflow: 'hidden',
+        clip: 'rect(0 0 0 0)'
+      }}
+      key={message} // Ensure React recreates element on message change
+      id={message}
+    >
+      {message}
+    </div>
+  );
+};
