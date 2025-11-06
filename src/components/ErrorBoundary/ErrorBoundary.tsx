d7813c42c3bf25f586834d46cc1d99df86ae7462
Michele Moio <michelemoiodev@gmail.com>
2025-11-06 15:36:08 +0100

    feat: [P4ADEV-3840] fiexes error & boundary

--- src/components/ErrorBoundary/ErrorBoundary.tsx
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,12 +1,13 @@
 import React, { ErrorInfo, ReactNode } from 'react';
 
 type ErrorBoundaryProps = {
-  children?: ReactNode;
+  children: ReactNode;
   fallback: ReactNode;
 };
 
 type ErrorBoundaryState = {
   hasError: boolean;
+  error?: Error;
 };
 
 class ErrorBoundary extends React.Component<
@@ -18,18 +19,45 @@ class ErrorBoundary extends React.Component<
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(): ErrorBoundaryState {
-    return { hasError: true };
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.error('Error boundary caught an error', error, errorInfo);
+    console.error('Error boundary caught an error:', error, errorInfo);
   }
 
-  render() {
-    return (
-      <>{this.state.hasError ? this.props.fallback : this.props?.children}</>
+  componentDidMount() {
+    // Catch unhandled promise rejections
+    window.addEventListener('unhandledrejection', this.handlePromiseRejection);
+    // Catch global errors
+    window.addEventListener('error', this.handleError);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener(
+      'unhandledrejection',
+      this.handlePromiseRejection
     );
+    window.removeEventListener('error', this.handleError);
+  }
+
+  handlePromiseRejection = (event: PromiseRejectionEvent) => {
+    console.error('Unhandled promise rejection:', event.reason);
+    this.setState({ hasError: true, error: event.reason });
+  };
+
+  handleError = (event: ErrorEvent) => {
+    console.error('Global error:', event.error);
+    this.setState({ hasError: true, error: event.error });
+  };
+
+  render() {
+    if (this.state.hasError) {
+      return <>{this.props.fallback}</>;
+    } else {
+      return <>{this.props.children}</>;
+    }
   }
 }
 
