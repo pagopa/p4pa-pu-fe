30a79be2a148aeb9647a7a4d865b94c689d9f387
EmVal <emanuele.valentini@nttdata.com>
2025-12-17 11:51:00 +0100

    use shared constant for storage key prefix

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,12 +1,10 @@
-const PRESERVED_KEY_PREFIXES = ['userProfilePreference:'];
+import { STORAGE_KEY_PREFIX } from './userPreferences';
 
 const clear = () => {
   window.sessionStorage.clear();
 
   Object.keys(window.localStorage)
-    .filter(
-      (key) => !PRESERVED_KEY_PREFIXES.some((prefix) => key.startsWith(prefix))
-    )
+    .filter((key) => !key.startsWith(STORAGE_KEY_PREFIX))
     .forEach((key) => {
       window.localStorage.removeItem(key);
     });
