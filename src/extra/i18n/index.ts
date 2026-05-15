cd5a9eb0f2185a05a7d14282ccc1d418f66df0d9
Michele Moio <michelemoiodev@gmail.com>
2026-05-15 08:51:44 +0200

    feat: [P4ADEV-4698] addon poc WIP

--- src/extra/i18n/index.ts
+++
@@ -1,8 +0,0 @@
-import i18n from '@core/translations/i18n';
-import translationIT from './it/translations.json';
-
-export const enterpriseTranslations = {
-  it: translationIT
-};
-
-i18n.addResourceBundle('it', 'send', enterpriseTranslations.it);
