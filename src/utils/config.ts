6fcf6cd82f79621a3d71b273db3b1794e90a9262
Vito Falco <vitofalco@gmail.com>
2026-08-26 14:31:11 +0200

    Rename APIHOST for FILESHARE

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,7 @@ import { CustomParamsSerializer } from 'axios';
 const {
   VITE_DEPLOY_PATH = '/piattaformaunitaria',
   VITE_APIHOST = 'http://localhost',
-  VITE_FILESHARE_APIHOST = 'http://localhost',
+  VITE_APIHOST_FILESHARE = 'http://localhost',
   VITE_API_TIMEOUT = '10000',
   VITE_LOGIN_URL = '/login',
   VITE_RESOURCES_URL = '/piattaformaunitaria-legaldocs/{BROKER_EXTERNAL_ID}/{DOCUMENT_TYPE}/{DOC_LANGUAGE}_{DOCUMENT_TYPE}.md'
@@ -19,14 +19,14 @@ const PARSED_API_TIMEOUT = Number.parseInt(VITE_API_TIMEOUT, 10);
 
 const DEPLOY_PATH_schema = z.string();
 const APIHOST_schema = z.string().url();
-const FILESHARE_APIHOST_schema = z.string().url();
+const APIHOST_FILESHARE_schema = z.string().url();
 const API_TIMEOUT_schema = z.number();
 const RESOURCES_URL_schema = z.string().min(1);
 
 try {
   DEPLOY_PATH_schema.parse(VITE_DEPLOY_PATH);
   APIHOST_schema.parse(import.meta.env.VITE_APIHOST);
-  FILESHARE_APIHOST_schema.parse(import.meta.env.VITE_FILESHARE_APIHOST);
+  APIHOST_FILESHARE_schema.parse(import.meta.env.VITE_APIHOST_FILESHARE);
   API_TIMEOUT_schema.parse(PARSED_API_TIMEOUT);
   RESOURCES_URL_schema.parse(import.meta.env.VITE_RESOURCES_URL);
 } catch (e) {
@@ -70,7 +70,7 @@ const config: Config = {
    * in the package.json file
    **/
   baseURL: VITE_APIHOST,
-  fileshareURL: VITE_FILESHARE_APIHOST,
+  fileshareURL: VITE_APIHOST_FILESHARE,
   deployPath: VITE_DEPLOY_PATH,
   loginUrl: VITE_LOGIN_URL,
   resourcesUrl: VITE_RESOURCES_URL,
