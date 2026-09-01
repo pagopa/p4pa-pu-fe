f5976f2b580edb7c1c26c59cbc22ac62fa9125da
Vito Falco <vitofalco@gmail.com>
2026-09-01 15:04:14 +0200

    Refact env sh

--- env.sh
+++ env.sh
@@ -3,24 +3,16 @@
 # Removing existing env file
 > .env
 
-# saving variables environment prefix in a variable
-varprefix="VITE_"
+env
 
-# Print relevant environment variables
-env | grep -i $varprefix
-
-# Recreate config file and assignment
-
-# Loop on environment variables prefixed with
-# PU_ and add them to .env file
-for var in $(env | grep "$varprefix"); do
-    varname=$(printf '%s\n' "${var#$varprefix}" | sed -e 's/=.*//')
-    varvalue=$(printf '%s\n' "$var" | sed -e 's/^[^=]*=//')
-
-    echo "$varname=\"$varvalue\"" >> .env
+# Loop on all environment variables and add them to .env file
+env | while IFS='=' read -r varname varvalue; do
+    if [ -n "$varname" ]; then
+        echo "$varname=\"$varvalue\"" >> .env
+    fi
 done
 
 # Print content of populated .env file
-echo -e "\n--- Contenuto di .env ---"
+echo -e "\n--- .env Contents ---"
 cat .env
 
